e3c01ce7264a3cc9ab3586bd8d735fc9abcd4818
HanQiGod <1539841933@qq.com>
2018-11-21 10:36:22 +0800

    Initial Commit

---
+++ HQWaveEffectDemoTests/HQWaveEffectDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  HQWaveEffectDemoTests.m
+//  HQWaveEffectDemoTests
+//
+//  Created by 君凯商联网 on 2018/11/21.
+//  Copyright © 2018 君凯商联网. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HQWaveEffectDemoTests : XCTestCase
+
+@end
+
+@implementation HQWaveEffectDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
